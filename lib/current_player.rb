8b116a2a801da42fdf9e9cb8519d563f70426f62
Marcelo Martinez <marcelomst1@gmail.com>
2020-02-25 19:47:19 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,7 @@
 def turn_count(board)
   board.each do |cell|
-    count += 1 if cell == "X" || cell == "o"
+    count = 0
+    count += 1 if cell == "X" || cell == "O"
   end
 end 
 def current_player(board)
